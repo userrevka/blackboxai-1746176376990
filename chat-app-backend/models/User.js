c9f47d788da217bdb3e2bf2514e2bcce8de271b3
userrevka <eliyaiguess@gmail.com>
2025-05-02 11:59:40 +0300

    Add chat-app-backend/models/User.js 1746176381153

---
+++ chat-app-backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  friends: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
